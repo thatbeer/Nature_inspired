7a51d106dee4e71cd85aafa4785c6cdfe45b182e
thatbeer <88705394+thatbeer@users.noreply.github.com>
2023-10-16 10:05:43 +0100

    update function

--- project01/core/genetic_fn.py
+++ project01/core/genetic_fn.py
@@ -1,4 +1,4 @@
-import math
+from typing import Optional, Literal
 import numpy as np
 import torch.nn as nn
 
@@ -23,7 +23,7 @@ def tournament_selection(population, fitness_value, tournament_size):
     res = None
     for _ in range(tournament_size):
         idx = np.random.randint(len(population))
-        # new_fit = fitness(population[idx], distance_metric=weight_metric)
+        # new_fit = fitness(population[idx], distance_metric=distance_metric)
         new_fit = fitness_value[idx]    
         if new_fit >= fit_value:
             fit_value = new_fit
@@ -31,6 +31,50 @@ def tournament_selection(population, fitness_value, tournament_size):
         
     return res, fit_value
 
+def crossover(arg1, arg2, locus:Optional[int]=None):
+    n = len(arg1)
+    if locus is None:
+        locus = np.random.randint(n-1)
+    child1 = arg1[:locus] + arg2[locus:] 
+    child2 = arg2[:locus] + arg1[locus:]
+    return child1, child2
 
+def mutate(arg):
+    """
+    Mutate (swap operator): Swapping between 2 random indexes in a list.
 
+    Args:
+        arg (list): The list to perform the mutation on.
 
+    Returns:
+        list: The mutated list with elements swapped.
+    """
+    n = len(arg)
+    if n < 2:
+        # Nothing to swap in a list with less than 2 elements
+        return arg
+    idx1, idx2 = np.random.choice(n, size=2, replace=False)
+    res = arg.copy()  
+    res[idx1], res[idx2] = res[idx2], res[idx1]  
+    return res
+
+def replace_firstweak(population, candidate, distance_metric):
+    pop_temp = population.copy()
+    cand_fitness = fitness(candidate, distance_metric)
+    for i, pop in enumerate(pop_temp):
+        if cand_fitness >= fitness(pop, distance_metric):
+            pop_temp[i] = candidate
+    return pop_temp
+
+def replace_weakest(population, candidate, distance_metric):
+    pop_temp = population.copy()
+    cand_fitness = fitness(candidate, distance_metric)
+    pop_fitness = [fitness(pop, distance_metric) for pop in pop_temp]
+    idx = np.argsort(pop_fitness)
+    for i in idx:
+        if pop_fitness[i] <= cand_fitness:
+            pop_temp[i] = candidate
+            return pop_temp
+        else:
+            return pop_temp
+    return pop_temp
