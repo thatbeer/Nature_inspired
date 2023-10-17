4db169bdcb74cef75088e916ce28d5b8eee6f984
thatbeer <88705394+thatbeer@users.noreply.github.com>
2023-10-17 17:05:36 +0100

    add mutate.py

--- project01/core/mutation.py
+++ project01/core/mutation.py
@@ -16,3 +16,34 @@ class Mutation:
     def multi_swap(self, candidate):
         pass
 
+def mutate(arg):
+    n = len(arg)
+    if n < 2:
+        return arg
+    idx1, idx2 = np.random.choice(n, size=2, replace=False)
+    res = arg.copy()  
+    res[idx1], res[idx2] = res[idx2], res[idx1]  
+    return res
+
+def multiple_mutate(arg, num_mutate:int=2):
+    temp = arg.copy()
+    for _ in range(num_mutate):
+        temp = mutate(temp)
+    return temp
+
+def inversion(candidate):
+    temp = candidate.copy()
+    n = len(temp)
+    idx1, idx2 = np.random.choice(n, size=2, replace=False)
+    cutoff = temp[idx1:idx2+1]
+    temp[idx1:idx2] = cutoff[::-1]
+    return temp
+
+def scramble(candidate):
+    temp = candidate.copy()
+    n = len(temp)
+    idx1, idx2 = np.random.choice(n, size=2, replace=False)
+    scrambled = np.random.shuffle(temp[idx1:idx2+1])
+    temp[idx1:idx2+1] = scrambled
+    return temp
+
